27f897d62af443e482ab9e718485dd1c7ad3a221
Elma Lopez <elmalopez@Elmas-MacBook-Pro.local>
2018-11-11 18:56:58 -0800

    here goes nothing

---
+++ assets/javascript/game.js
@@ -0,0 +1,71 @@
+
+//establlish array and variables
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var userChoice = [];
+var randomCharacterVar;
+var computerChoice;
+var wins = 0;
+var losses = 0; 
+var guessesLeft = 9;
+var guessesSoFarVar = [];
+
+// console.log(options);
+
+//call function for computer choice
+randomCharacter();
+
+// //user chooses one character a-z and we capture those choices
+document.onkeyup = function(event) {
+    var userChoice = String.fromCharCode(event.keyCode);
+
+        // .toLowerCase();
+        userChoice = userChoice.toLowerCase();
+
+    
+
+    // //compare choices and determine a win or loss or tie 
+    if (userChoice === computerChoice) {
+        alert("I can't believe it...you guessed it, my darling!");
+        wins = wins + 1;
+        document.getElementById("wins").innerHTML = wins;
+        randomCharacter();
+        guessesLeft = 9;
+        guessesSoFarVar = "Let's go again...you haven't guessed anything this time around";
+        document.getElementById("guessesSoFar").innerHTML = guessesSoFarVar;
+        guessesSoFarVar = [];
+    } else if (guessesLeft > 0){
+        
+        guessesSoFarVar.push(" " + userChoice);
+        document.getElementById("guessesSoFar").innerHTML = guessesSoFarVar;
+        guessesLeft = guessesLeft - 1;
+        document.getElementById("guessesLeft").innerHTML = guessesLeft;
+        
+
+    }
+    else {
+        // increment losses print loses to screen 
+        losses = losses + 1;
+        document.getElementById("losses").innerHTML = losses;
+
+        //restart game
+        randomCharacter();
+        //reset guesses left to 9
+        guessesLeft = 9;
+        guessesSoFarVar = "Try again";
+        document.getElementById("guessesSoFar").innerHTML = guessesSoFarVar;
+        guessesSoFarVar = [];
+        
+    }
+  
+    }
+    
+
+
+
+// //computer chooses one random character a-z - used math random formula to create computer choice (computer to make a choice)
+function randomCharacter() {
+    var randomNumber = Math.floor(Math.random() * 
+        options.length);
+    computerChoice = options[randomNumber];
+    
+}
